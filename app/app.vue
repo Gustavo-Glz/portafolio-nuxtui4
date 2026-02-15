3a60d394beee1e13e5617e94d4a6a91dfbfd02ef
Gustavo-Glz <gustavo.gonzalez.montalvo@gmail.com>
2026-02-14 19:43:17 -0600

    update: add seo meta tags

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,21 @@
 <script setup lang="ts">
+useSeoMeta({
+  title: 'Gustavo González - Desarrollador Full Stack',
+  description:
+    'Bienvenido a mi portafolio personal. Soy Gustavo González, un desarrollador Full Stack apasionado por crear soluciones innovadoras y eficientes.',
+  ogTitle: 'Gustavo González - Desarrollador Full Stack',
+  ogDescription:
+    'Bienvenido a mi portafolio personal. Soy Gustavo González, un desarrollador Full Stack apasionado por crear soluciones innovadoras y eficientes.',
+  ogType: 'website',
+  ogUrl: 'https://gustavogonzalez.dev',
+  ogImage: '/og-image.png',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Gustavo González - Desarrollador Full Stack',
+  twitterDescription:
+    'Bienvenido a mi portafolio personal. Soy Gustavo González, un desarrollador Full Stack apasionado por crear soluciones innovadoras y eficientes.',
+  twitterImage: '/og-image.png'
+})
+
 onBeforeMount(() => {
   if (window.location.hash) {
     history.replaceState(null, '', window.location.pathname)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'es-MX'
+      }
+    }
+  },
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
   css: ['~/assets/css/main.css']
 })
